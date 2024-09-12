8d03f0b142d33dd0e0e6c99f78b30522579b2075
Umrzoq0323 <umrzoqaraqulov01@gmail.com>
2024-09-12 17:41:49 +0500

    added about page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
-const bar = document.querySelector(".header__nav__btns-bar");
-const headerBtnClose = document.querySelector(".header__nav__btn-close");
-const headerListClose = document.querySelector(".header__list-close");
+const barBtn = document.querySelector(".header__nav__btns__btn-bar");
+const headerListClose = document.querySelector(".header__nav__close");
+const headerList = document.querySelector(".header__nav__list");
 
-bar.addEventListener("click", function () {
-    headerList.classList.toggle
-    ("header__list-show");
-    
-})
+barBtn.addEventListener("click", function () {
+  headerList.classList.toggle("header__nav__list-show");
+});
 
 headerListClose.addEventListener("click", function () {
-    headerList.classList.remove
-    ("header__list-show")
-})
+  headerList.classList.remove("header__nav__list-show");
+});
